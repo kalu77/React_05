98c5b10252301b8639b5eb96f2a92c2698bf5d96
kalu77 <kalujerovich77@hotmail.com>
2020-09-27 15:36:42 -0300

    update

---
+++ app.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      estudios: 'secundario'
+    }
+    this.cambioEstudios = this.cambioEstudios.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <p><input type="radio" value="primario" checked={this.state.estudios==='primario'}
+          onChange={this.cambioEstudios} />Primario</p>
+        <p><input type="radio" value="secundario" checked={this.state.estudios==='secundario'}
+          onChange={this.cambioEstudios} />Secundario</p>
+        <p><input type="radio" value="universitario" checked={this.state.estudios==='universitario'}
+          onChange={this.cambioEstudios} />Universitario</p>
+        <p>Estudio seleccionado: {this.state.estudios}</p>
+      </div>
+    );
+  }
+
+  cambioEstudios(e) {
+    this.setState({
+      estudios: e.target.value
+    })
+  }
+}
+
+export default App;
